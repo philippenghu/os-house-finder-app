273f0152bb169e63bc8abc5d6e50a8713040bc50
philippenghu <ph174175275@gmail.com>
2023-09-09 23:01:38 -0600

    Add the About component

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
+import ProfessionalCard from './ProfessionalCard'
 
 
 function About() {
-    return (
-        <div className='container-md pt-4'>
-          <h1>About us</h1>
-          
-        </div>
-    );
+  return (
+    <>
+      <h1 className='text-center'>About us</h1>
+      <div className='container-xl pt-4 d-flex  justify-content-center'>
+     
+          <ProfessionalCard name='Employee 1' title='Frontend developer' description='1st Employee' image='images/Shirt.png' />
+          <ProfessionalCard name='Employee 2' title='Backend developer' description='2st Employee' image='images/Shirt.png' />
+          <ProfessionalCard name='Employee 3' title='Product manager' description='3st Employee' image='images/Shirt.png' />
+          <ProfessionalCard name='Employee 4' title='UX designer' description='4st Employee' image='images/Shirt.png' />
+        
+      </div>
+    </>
+  );
 }
 
 export default About;
